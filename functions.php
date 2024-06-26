2c1149bf1ddfb70ec337be332a9b07f7dfa87ad9
rlewisauthor <fistsoffiction@gmail.com>
2024-06-26 00:50:18 -0700

    Delete functions.php

--- functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-function my_custom_theme_setup() {
-    register_nav_menus( array(
-        'header-menu' => 'Header Menu'
-    ) );
-}
-add_action( 'after_setup_theme', 'my_custom_theme_setup' );
-?>
